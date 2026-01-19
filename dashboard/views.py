2769d5726ec6593663ad6a40e16ca6d513b165cc
isaltosf <alexandersaltosp@gmail.com>
2026-01-18 21:55:22 -0500

    guia 26

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests 
+import requests
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
+
 
 @login_required
+@permission_required("dashboard.index_viewer", raise_exception=True)
 def index(request):
     response = requests.get(settings.API_URL)
     posts = response.json()
     total_responses = len(posts)
     data = {
-    'title': "Landing Page' Dashboard",
-    'total_responses': total_responses,
+        "title": "Landing Page' Dashboard",
+        "total_responses": total_responses,
     }
-    return render(request, 'dashboard/index.html', data)
-
+    return render(request, "dashboard/index.html", data)
